77e66409c96ad829fddc87d196ac6d66f8ea9f6d
Adéla Boubelíková <adela.boubelikova@skolaeupraha.cz>
2025-05-22 09:34:24 +0200

    React- HW2 commit

--- React-ukol/react-app/src/App.jsx
+++ React-ukol/react-app/src/App.jsx
@@ -1,8 +1,15 @@
 import Header from './header'
+import Footer from './footer' 
+import Food from './Food'
 
 function App() {
  return (
-  <Header></Header>
+  <>
+    <Header/>
+    <Food/>
+    <Footer/>  {/* jednotlivé sekce, které mam naproramované všude možné a pak jsou jen spojeni do app.jsx.*/}
+    {/* to jak je to tady poskládané za sebou, tak tak se to zobrazí i v prohlížeči */}
+  </>
  )
 }
 
